b5fe60ddf8559f988790d3ce6551a60508efe08a
hydroxy42 <farhan2262003@gmail.com>
2024-11-23 16:01:19 +0530

    initial commit

---
+++ src/app/manifest.js
@@ -0,0 +1,28 @@
+export default function manifest() {
+    return {
+      name: 'Next.js PWA',
+      short_name: 'NextPWA',
+      description: 'A Progressive Web App built with Next.js',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: '/web-app-manifest-144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: '/web-app-manifest-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+            src: '/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+        },
+      ],
+    }
+  }
---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import InstallPrompt from './_components/IosInstall';
+import PushNotificationManager from './_components/SubscriptionManager';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Welcome to Push Notifications App</h2>
+      <p>
+        This app allows you to subscribe to push notifications and experience the benefits of a Progressive Web App (PWA).
+      </p>
+      <PushNotificationManager />
+      <InstallPrompt/>
+    </div>
+  );
+}
